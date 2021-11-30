1fb11da4bfd991dce4c5b619d0417ff143751926
adri <adriibelenaguirre@gmail.com>
2021-11-30 14:18:33 -0300

    primer commit

---
+++ catalogo/catalogo/settings/production.py
@@ -0,0 +1,10 @@
+DEBUG = True
+
+ALLOWED_HOSTS = ['midomininio.com.ar']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
